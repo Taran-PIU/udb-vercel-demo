809239168a3b1f0ac7265ed7e8e5bfdbed6f962d
Taran-PIU <taran.mesala@prakashinnovativeuniverse.com>
2025-03-03 14:06:48 +0530

    fixed cors-v1

--- backend/server.js
+++ backend/server.js
@@ -126,10 +126,10 @@ mongoose
   
 
 
-app.use(express.static(path.resolve(__dirname,"../frontend/build")));
+app.use(express.static(path.resolve(__dirname,"build")));
 
 app.get("*",(req,res)=>{
-  res.sendFile(path.resolve(__dirname,"../frontend/build","index.html"));
+  res.sendFile(path.resolve(__dirname,"build","index.html"));
 });
 
 //Routes
